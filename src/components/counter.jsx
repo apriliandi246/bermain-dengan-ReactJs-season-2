e7640927543e80b8580fee5fe60554ccdc489af3
apriliandi <apriliandi642@gmail.com>
2020-06-10 02:11:56 +0700

    Setting Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
    state = {
-      count: 0
+      count: 0,
+      imageUrl: 'https://picsum.photos/200'
    };
+
+   styles = {
+      fontWeight: 'bold',
+      fontSize: 10,
+      textAlign: 'center',
+      marginTop: 20
+   }
 
    render() {
       return (
          <div>
-            <span>{this.formatCount()}</span>
-            <button className="btn btn-primary">Increment</button>
+            {/* <img src={this.state.imageUrl} alt="" /> */}
+            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+            <p style={this.styles}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente suscipit delectus non doloribus officiis reprehenderit dignissimos consequatur voluptatem saepe iusto explicabo corrupti, commodi laudantium officia ea minus dolor nemo molestiae!</p>
          </div>
       );
    }
