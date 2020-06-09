454e3359ed18121f34019e7f8c7a530554c98bcb
apriliandi <apriliandi642@gmail.com>
2020-06-10 03:39:36 +0700

    Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,22 +9,15 @@ class Counter extends Component {
    render() {
       return (
          <div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+            {this.renderTags()}
          </div>
       );
    }
 
-   getBadgeClasses() {
-      let classes = "badge m-2 badge-";
-      classes += (this.state.count === 0) ? "warning" : "primary";
-      return classes;
-   }
+   renderTags() {
+      if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-   formatCount() {
-      const { count } = this.state;
-      return count === 0 ? 'zero' : count;
+      return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
    }
 }
 
