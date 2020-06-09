9ce7a42e283c00c75ac5ced018d376667a234c6c
apriliandi <apriliandi642@gmail.com>
2020-06-10 02:19:46 +0700

    Rendering Classes Dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,22 +6,19 @@ class Counter extends Component {
       imageUrl: 'https://picsum.photos/200'
    };
 
-   styles = {
-      fontWeight: 'bold',
-      fontSize: 10,
-      textAlign: 'center',
-      marginTop: 20
-   }
-
    render() {
       return (
          <div>
-            {/* <img src={this.state.imageUrl} alt="" /> */}
-            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
-            <p style={this.styles}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente suscipit delectus non doloribus officiis reprehenderit dignissimos consequatur voluptatem saepe iusto explicabo corrupti, commodi laudantium officia ea minus dolor nemo molestiae!</p>
          </div>
       );
+   }
+
+   getBadgeClasses() {
+      let classes = "badge m-2 badge-";
+      classes += (this.state.count === 0) ? "warning" : "primary";
+      return classes;
    }
 
    formatCount() {
